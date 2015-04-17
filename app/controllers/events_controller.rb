4179ed2809fef85bd5fd7d85980bb7eea81aeedb
Dominic90 <dr@web-computing.de>
2015-04-17 23:26:27 +0200

    added basic event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,42 @@
 class EventsController < ApplicationController
+	def index
+		@events = Event.all
+	end
+
+	def show
+    	@event = Event.find(params[:id])
+	end
+
 	def new
+		@event = Event.new
+	end
+
+	def edit
+		@event = Event.find(params[:id])
 	end
 
 	def create
-		render plain: params[:event].inspect
+		@event = Event.new(event_params)
+ 
+	 	if @event.save
+    		redirect_to @event
+		else
+    		render 'new'
+		end
 	end
+
+	def update
+		@event = Event.find(params[:id])
+ 
+		if @event.update(event_params)
+			redirect_to @event
+		else
+			render 'edit'
+		end
+end
+
+	private
+	 	def event_params
+	    	params.require(:event).permit(:title, :description)
+		end
 end
