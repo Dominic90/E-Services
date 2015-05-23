ae8ae1127767e7295849d89831909daf878488e2
Dominic90 <dr@web-computing.de>
2015-05-23 19:31:51 +0200

    added some elements for kurento

--- rails/app/controllers/events_controller.rb
+++ rails/app/controllers/events_controller.rb
@@ -1,6 +1,30 @@
 class EventsController < ApplicationController
+
+	require 'socket'
+
 	def index
 		@events = Event.all
+
+		s = TCPSocket.new 'localhost', 11111
+		s.puts "hello"
+		s.puts "world"
+		puts s.gets
+		s.close
+
+		#uri = URI('http://localhost:11111')
+		#req = Net::HTTP::Post.new(uri)
+		#req.set_form_data('from' => '2005-01-01', 'to' => '2005-03-31')
+
+		#res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+		#  http.request(req)
+		#end
+
+		#case res
+		#when Net::HTTPSuccess, Net::HTTPRedirection
+		  # OK
+		#else
+		#  res.value
+		#end
 	end
 
 	def show
